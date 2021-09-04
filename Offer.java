4eddb6e7bd2755a6cbb895f9ce6dbd61ce3cffa9
lavanyachebrolu <89945738+lavanyachebrolu@users.noreply.github.com>
2021-09-04 08:34:00 +0530

    Delete Offer.java

--- Offer.java
+++
@@ -1,7 +0,0 @@
-package com.lavanya.assignment1;
-
-public interface Offer {
-	public void addDealSubcriber(Customer customer);
-	public void removeDealSubcriber(Customer customer);
-	public void notifyDealSubscribers(String message);
-}
